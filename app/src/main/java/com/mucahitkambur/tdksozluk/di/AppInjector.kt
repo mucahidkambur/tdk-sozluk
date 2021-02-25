4fce4e025ce49e86c4966b421660f648bc2f5087
Mücahid KAMBUR <mucahidkambur@gmail.com>
2021-02-26 01:32:26 +0300

    Update compileSdkVersion and versionCode

--- app/src/main/java/com/mucahitkambur/tdksozluk/di/AppInjector.kt
+++ app/src/main/java/com/mucahitkambur/tdksozluk/di/AppInjector.kt
@@ -41,7 +41,7 @@ object AppInjector {
 
             }
 
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
 
             }
 
